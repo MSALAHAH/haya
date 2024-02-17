273114f6cc9af46a364b0040f3d9f5f2a0408601
Mohammed Salah <mohammedmelok001@gmail.com>
2024-02-17 10:32:47 +0200

    productButtonUpdate

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobil
 
 // Close mobile menu on close button click
 closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
+function shareProduct(productCard) {
+    // Find product name and description elements within the product card
+    var productName = productCard.querySelector('.product-name').innerText;
+    var productDescription = productCard.querySelector('.product-description').innerText;
+  
+    // Construct the message to share
+    var message = "Check out this product: " + "\n" + productName + "\n" + productDescription;
+  
+    // Open WhatsApp with pre-filled message
+    var whatsappURL = "https://wa.me/?text=" + encodeURIComponent(message);
+    window.open(whatsappURL);
+  
+    // For Telegram, you would construct a similar URL
+    // var telegramURL = "https://t.me/share/url?url=&text=" + encodeURIComponent(message);
+    // window.open(telegramURL);
+  }
+  
