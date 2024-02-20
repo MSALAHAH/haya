388593864903d0f7a8f47cd6fb647b1a0b0385a0
Mohammed Salah <mohammedmelok001@gmail.com>
2024-02-20 19:05:52 +0200

    tabsUpdate

--- js/script.js
+++ js/script.js
@@ -9,19 +9,51 @@ hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobil
 closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 
 function shareProduct(productCard) {
-    // Find product name and description elements within the product card
-    var productName = productCard.querySelector('.product-name').innerText;
-    var productDescription = productCard.querySelector('.product-description').innerText;
-  
-    // Construct the message to share
-    var message = "Check out this product: " + "\n" + productName + "\n" + productDescription;
-  
-    // Open WhatsApp with pre-filled message
-    var whatsappURL = "https://wa.me/?text=" + encodeURIComponent(message);
-    window.open(whatsappURL);
-  
-    // For Telegram, you would construct a similar URL
-    // var telegramURL = "https://t.me/share/url?url=&text=" + encodeURIComponent(message);
-    // window.open(telegramURL);
-  }
-  
+  // Find product name and description elements within the product card
+  var productName = productCard.querySelector('.product-name').innerText;
+  var productDescription = productCard.querySelector('.product-description').innerText;
+
+  // Construct the message to share
+  var message = "Check out this product: " + "\n" + productName + "\n" + productDescription;
+
+  // Open WhatsApp with pre-filled message
+  var whatsappURL = "https://wa.me/?text=" + encodeURIComponent(message);
+  window.open(whatsappURL);
+
+  // For Telegram, you would construct a similar URL
+  // var telegramURL = "https://t.me/share/url?url=&text=" + encodeURIComponent(message);
+  // window.open(telegramURL);
+}
+
+//the tab code
+
+function showTab(targetId) {
+  const tabs = document.querySelectorAll('.tab');
+  const tabContents = document.querySelectorAll('.tab-content');
+
+  tabs.forEach(tab => {
+      tab.classList.remove('active');
+  });
+  tabContents.forEach(content => {
+      content.classList.remove('active');
+  });
+
+  const tab = document.querySelector(`[data-target="${targetId}"]`);
+  const content = document.querySelector(targetId);
+
+  tab.classList.add('active');
+  content.classList.add('active');
+}
+
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+      const target = tab.dataset.target;
+      showTab(target);
+  });
+});
+
+
+
